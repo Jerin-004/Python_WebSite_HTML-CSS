7971a005295f617c4ae8a1fe10e0238914e28c70
Chijith Jerin <chijithjerin2004@gmail.com>
2021-11-30 07:36:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,42 @@
 // This is my first javascript code
-console.log("Hello World");
+// console.log("Hello World");
+
+
+// These are primitive types
+let name = "Jerin";   // String literal
+console.log(name);
+let age = 17;  // Number literal
+let isApproved = false   // Boolean literal
+let middleName = undefined;
+let nickName = null;
+
+
+// They cannot be a reserved keyword
+// They should be meaningful
+// they cannot start with a number (1name)
+// They cannot contain a space or a hypen (-)
+// Are case-sensitive
+
+// This is called camel notation
+let firstName = "Chijith"
+let lastName = "Jerin";
+
+const intrestRate = 0.3;
+console.log(intrestRate);
+
+
+// Reference types
+
+let person = {
+    name: "Jerin",
+    age: 18
+}; // Object literal
+
+// Dot notation
+person.name = "Cj";
+
+// Braacet notation
+person["age"] = "18";
+
+console.log(person.name);
+console.log(person.age)
